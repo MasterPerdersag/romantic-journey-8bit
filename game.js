43776bc0f1fd542952b4f282e4acb13a475d48af
MasterPerdersag <yannicankum@gmail.com>
2025-04-23 13:50:46 +0200

    fix: scale background image to cover screen

--- game.js
+++ game.js
@@ -12,30 +12,26 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-  // Korrekte Pfadangabe mit exaktem Dateinamen und Groß-/Kleinschreibung
   this.load.image('background', 'assets/images/bg1.png.PNG');
 }
 
 function create() {
-  // Hintergrundbild zentriert anzeigen
   const bg = this.add.image(400, 300, 'background');
-  bg.setOrigin(0.5, 0.5);
+  // Skaliert das Bild so, dass es die gesamte Fläche ohne schwarze Balken füllt
+  bg.setScale(Math.max(800 / bg.width, 600 / bg.height));
 
-  // Englischer Text
   this.add.text(80, 30, 'Happy Birthday\nWelcome to our little adventure', {
     font: '24px Arial',
     fill: '#ffffff',
     align: 'left'
   });
 
-  // Persischer Text
   this.add.text(80, 120, 'تولدت مبارک\nبه ماجراجویی کوچک ما خوش آمدی', {
     font: '24px Arial',
     fill: '#ffcc00',
     align: 'left'
   });
 
-  // Continue-Button
   const button = this.add.rectangle(400, 500, 220, 50, 0x2ecc71).setInteractive();
   const buttonText = this.add.text(0, 0, 'Continue', {
     font: '20px Arial',
@@ -45,7 +41,6 @@ function create() {
 
   button.on('pointerdown', () => {
     alert('Das Spiel beginnt gleich...');
-    // hier kannst du z.B. eine neue Szene starten
+    // weitere Szene hier starten
   });
 }
-
