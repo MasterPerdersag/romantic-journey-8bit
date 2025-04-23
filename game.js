a3c35ccb681a2537f49ef7eede6655de8ca67750
MasterPerdersag <yannicankum@gmail.com>
2025-04-23 14:34:46 +0200

    Update game.js

--- game.js
+++ game.js
@@ -21,9 +21,11 @@ function create() {
   bg.setOrigin(0, 0);
   bg.setDisplaySize(this.sys.game.config.width, this.sys.game.config.height);
 
-  // Button ganz unten zentriert
-  const buttonWidth = 220;
-  const buttonHeight = 50;
+  // Button-Daten
+  const buttonWidth = 146;
+  const buttonHeight = 33;
+
+  // Button erstellen
   const button = this.add.rectangle(
     config.width / 2,
     config.height - (buttonHeight / 2),
@@ -32,13 +34,17 @@ function create() {
     0x2ecc71
   ).setInteractive();
 
+  // Text mit Schatten
   const buttonText = this.add.text(0, 0, 'Continue', {
-    font: '20px Arial',
+    font: '18px Arial',
     fill: '#ffffff'
   });
+  buttonText.setShadow(2, 2, '#000000', 2, false, true);
   Phaser.Display.Align.In.Center(buttonText, button);
 
+  // Klickfunktion
   button.on('pointerdown', () => {
     alert('Das Spiel beginnt gleich...');
   });
 }
+
