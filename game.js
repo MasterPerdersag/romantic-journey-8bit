8f36f609e377adf6039d000e4af86fb4a73e81e0
MasterPerdersag <yannicankum@gmail.com>
2025-04-23 13:43:31 +0200

    fix: background image path and button setup

--- game.js
+++ game.js
@@ -4,47 +4,48 @@ const config = {
   height: 600,
   parent: 'game-container',
   scene: {
-    preload: preload,
-    create: create
+    preload,
+    create
   }
 };
 
 const game = new Phaser.Game(config);
 
 function preload() {
-  // Stelle sicher, dass der Bildname exakt so geschrieben ist!
+  // Korrekte Pfadangabe mit exaktem Dateinamen und Groß-/Kleinschreibung
   this.load.image('background', 'assets/images/bg1.png.PNG');
 }
 
 function create() {
   // Hintergrundbild zentriert anzeigen
-  this.add.image(400, 300, 'background').setOrigin(0.5);
+  const bg = this.add.image(400, 300, 'background');
+  bg.setOrigin(0.5, 0.5);
 
   // Englischer Text
-  this.add.text(100, 40, 'Happy Birthday\nWelcome to our little adventure', {
+  this.add.text(80, 30, 'Happy Birthday\nWelcome to our little adventure', {
     font: '24px Arial',
     fill: '#ffffff',
-    align: 'center'
+    align: 'left'
   });
 
   // Persischer Text
-  this.add.text(100, 120, 'تولدت مبارک\nبه ماجراجویی کوچک ما خوش آمدی', {
+  this.add.text(80, 120, 'تولدت مبارک\nبه ماجراجویی کوچک ما خوش آمدی', {
     font: '24px Arial',
     fill: '#ffcc00',
-    align: 'center'
+    align: 'left'
   });
 
   // Continue-Button
-  const button = this.add.rectangle(400, 500, 200, 50, 0x008866).setInteractive();
+  const button = this.add.rectangle(400, 500, 220, 50, 0x2ecc71).setInteractive();
   const buttonText = this.add.text(0, 0, 'Continue', {
     font: '20px Arial',
     fill: '#ffffff'
   });
   Phaser.Display.Align.In.Center(buttonText, button);
 
-  // Button-Aktion
   button.on('pointerdown', () => {
-    alert('Spiel beginnt!');
-    // Hier kannst du später eine neue Scene starten
+    alert('Das Spiel beginnt gleich...');
+    // hier kannst du z.B. eine neue Szene starten
   });
 }
+
