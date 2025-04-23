0b177a26c2c30a2855731914989c63bcd769135d
MasterPerdersag <yannicankum@gmail.com>
2025-04-23 14:42:01 +0200

    Update game.js

--- game.js
+++ game.js
@@ -16,23 +16,21 @@ function preload() {
 }
 
 function create() {
-  // Hintergrundbild
-  const bg = this.add.image(0, 0, 'background');
-  bg.setOrigin(0, 0);
-  bg.setDisplaySize(config.width, config.height);
+  // Hintergrundbild im Originalmaßstab zentriert anzeigen
+  const bg = this.add.image(config.width / 2, config.height / 2, 'background');
+  bg.setOrigin(0.5, 0.5);
 
-  // Transparenter Button genau auf dem grafischen "Continue"
+  // Transparenter Button exakt auf dem Continue-Bereich im Bild
   const button = this.add.rectangle(
-    256,  // Mitte des Bildes (X)
-    720,  // Y-Position knapp über dem unteren Rand, exakt auf dem Bild-Button
-    140,  // Breite passend zum Button im Bild
-    36,   // Höhe des Buttons
+    256,  // X-Mitte
+    725,  // Y-Position manuell angepasst
+    140,  // Breite
+    36,   // Höhe
     0xffffff,
-    0.001 // Fast komplett transparent
+    0.001 // Unsichtbar
   ).setInteractive();
 
   button.on('pointerdown', () => {
     alert('Let the adventure begin...');
-    // Hier kannst du später zur nächsten Szene wechseln
   });
 }
