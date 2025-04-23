0b3f5082b0b7acf623ede1d4a4f5266045f2003a
MasterPerdersag <yannicankum@gmail.com>
2025-04-23 21:09:56 +0200

    Update game.js

--- game.js
+++ game.js
@@ -15,32 +15,32 @@ const game = new Phaser.Game(config);
 
 function preload() {
   this.load.image('background', 'assets/images/bg1.png.PNG');
-  this.load.audio('bgmusic', ['assets/assets/audio/music.mp3']);
+  this.load.audio('bgMusic', ['assets/assets/audio/music.mp3']);
 }
 
 function create() {
-  // Hintergrundbild anzeigen
+  // Hintergrundbild
   const bg = this.add.image(0, 0, 'background');
   bg.setOrigin(0, 0);
   bg.setDisplaySize(this.sys.game.config.width, this.sys.game.config.height);
 
-  // Musik laden
-  backgroundMusic = this.sound.add('bgmusic', {
+  // Musik abspielen
+  backgroundMusic = this.sound.add('bgMusic', {
     loop: true,
     volume: 0.5
   });
 
-  // Lautstärkeregler mit HTML verbinden
+  // Lautstärkeregler verbinden
   const volumeSlider = document.getElementById('volumeSlider');
-  if (volumeSlider) {
-    volumeSlider.addEventListener('input', () => {
-      const volume = parseFloat(volumeSlider.value);
+  volumeSlider.addEventListener('input', function () {
+    const volume = parseFloat(this.value);
+    if (backgroundMusic) {
       backgroundMusic.setVolume(volume);
-    });
-  }
+    }
+  });
 
-  // Unsichtbarer Button exakt unterhalb des sichtbaren "Continue"-Bereichs im Bild
-  const continueZone = this.add.zone(400, 750, 160, 30) // x, y, Breite, Höhe
+  // Unsichtbarer, klickbarer Bereich auf dem "Continue"-Feld im Bild
+  const continueZone = this.add.zone(400, 570, 160, 35) // <- hier die Position angepasst
     .setOrigin(0.5)
     .setInteractive({ useHandCursor: true })
     .on('pointerdown', () => {
