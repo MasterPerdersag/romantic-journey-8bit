db741ffc32d09cefa196be05aeb628a770ee10c1
MasterPerdersag <yannicankum@gmail.com>
2025-04-30 12:45:31 +0200

    Update game.js

--- game.js
+++ game.js
@@ -279,9 +279,13 @@ class EighthScene extends Phaser.Scene {
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
   parent: 'game-container',
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 800,
+    height: 600
+  },
   scene: [
     StartScene,
     SecondScene,
